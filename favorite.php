ae3ab659efc1965cd14014d0e6d0b229cea561ee
Tripti Chhetri <triptichhetri312@gmail.com>
2018-11-24 22:22:03 -0500

    changed the extension from html to php

---
+++ favorite.php
@@ -0,0 +1,230 @@
+<?php include('head.php'); ?>
+<body>
+<?php include('nav.php'); ?>
+ 
+ <!-- main body content -->   
+<div class="main">
+    <!--
+    <div class="section">
+        <img class="rounded mainImage" src="image/main.jpg" alt="main" height="400px" width="800px">
+        <div class="centerText">
+            <h5><b>We sell <span style="color: #FE8B92;">US Brand Cosmetics</span> Online!You can reserve the products online and get it picked up or delivered at your door.</b></h5>
+        </div>
+    </div>
+-->
+<!-- Carousel -->
+    <?php include('carousel.php'); ?>
+    
+    
+        <div class="container-fluid" style="margin-top:50px;">
+            <h4 class="text-center">Must Have Skincare</h4>
+            <div class="row" style="margin-top:30px;">
+                <div class="col-md-4" data-id="01" data-name="Cleanser" data-price="10.00">
+                    <img class="img-fluid mx-auto d-block skin" src="image/cleanser.jpg" alt="" width="400px" height="400px">
+                </div>
+                <div class="col-md-4" data-id="01" data-name="Cleanser" data-price="10.00">
+                    <img class="img-fluid mx-auto d-block skin" src="image/primer.jpg" alt="" width="400px" height="400px">
+                </div>
+                <div class="col-md-4" data-id="01" data-name="Cleanser" data-price="10.00">
+                    <img class="img-fluid mx-auto d-block skin" src="image/faceBalm.jpg" alt="" width="400px" height="400px">
+                </div>
+            </div>   
+            <div class="row">
+                <div class="col-md-4" data-id="01" data-name="Cleanser" data-price="10.00">
+                    <img class="img-fluid mx-auto d-block skin" src="image/moisturizer.jpg" alt="" width="400px" height="400px">
+                </div>
+                 <div class="col-md-4" data-id="01" data-name="Cleanser" data-price="10.00">
+                    <img class="img-fluid mx-auto d-block skin" src="image/nightCream.jpg" alt="" width="400px" height="400px">
+                </div>
+                <div class="col-md-4" data-id="01" data-name="Cleanser" data-price="10.00">
+                    <img class="img-fluid mx-auto d-block skin" src="image/drop.jpg" alt="" width="400px" height="400px">
+                </div>
+            </div>
+        </div>
+    
+<! Slanted Step Div ->
+    
+<div class="slants">
+    <div class="container-fluid">
+       <div class="row">
+            <div class="col-sm-2">
+                <div class="text-center">
+                <h3 >Step 1</h3>
+                <img src="image/cleanser.jpg" class="img-fluid step" alt="" width="">
+                <p></p>
+                </div>
+            </div>
+           <div class="col-sm-2">
+               <div class="text-center">
+                <h3>Step 2</h3>
+                <img src="image/moisturizer.jpg" class="img-fluid step" alt="" width="170">
+                <p></p>
+               </div>
+           </div>
+           <div class="col-sm-2">
+               <div class="text-center">
+                <h3>Step 3</h3>
+                <img src="image/primer.jpg" class="img-fluid step" alt="img-fluid step" width="">
+                <p></p>
+               </div>
+           </div>
+           <div class="col-sm-2">
+               <div class="text-center">
+                <h3>Step 4</h3>
+                <img src="image/tinted.jpg" class="img-fluid step" alt="img-fluid step" width="">
+                <p></p>
+               </div>
+           </div>
+           <div class="col-sm-2">
+               <div class="text-center">
+                <h3>Step 5</h3>
+                <img src="image/MadforMatte_EyeshadowPalettes.jpg" class="img-fluid step" alt="img-fluid step" width="">
+                <p></p>
+               </div>
+           </div>
+           <div class="col-sm-2">
+               <div class="text-center">
+                <h3>Step 6</h3>
+                <img src="image/liquidEyeliner.jpg" class="img-fluid step" alt="img-fluid step" width="">
+                <p></p>
+               </div>
+           </div>
+        </div>
+    </div>
+</div>
+    
+<div class="container-fluid" style="margin-top:50px;">
+    <h4 class="text-center" style="margin-bottom:20px;">Shop The Best Selling Products</h4>
+        <div class="row gallery1">
+            <div class="col-sm-3">
+                <div class="content">
+                    <div class="content-overlay"></div>
+                    <img class="img-responsive rounded mx-auto d-block collapsible images" src="image/MadforMatte_EyeshadowPalettes.jpg" alt="eyeshadow palette" width="200px" height="200px">
+                    <div class="content-details fadeIn-bottom">
+                        <p class="content-text">Rose Gold Eyeshadow Palette<br> <i class="fa fa-heart-o fa-2x" aria-hidden="true"></i> <i id="cart-btn" class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i></p>    
+                    </div>
+               </div>
+            </div>
+            
+            <div class="col-sm-3">
+                <div class="content">
+                    <div class="content-overlay"></div>                
+                    <img class="img-responsive rounded images" src="image/liquidEyeliner.jpg" alt="eyeshadow palette" width="200px" height="200px">
+                <figcaption class="figure-caption"></figcaption>
+                <div class="content-details fadeIn-bottom">
+                        <p class="content-text">Satin EyeLiner Pencil<br> <i class="fa fa-heart-o fa-2x" aria-hidden="true"></i>
+                        <i class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i></p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col-sm-3">
+                <div class="content">
+                    <div class="content-overlay"></div>
+                <img class="img-responsive rounded mx-auto d-block images" src="image/pencileEyeliner.jpg" alt="eyeshadow palette" width="200px" height="200px">
+                  <figcaption class="figure-caption"></figcaption>
+                 <div class="content-details fadeIn-bottom">
+                        <p class="content-text">Waterproof Liquid EyeLiner <br> <i class="fa fa-heart-o fa-2x" aria-hidden="true"></i> <i class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i></p>
+                </div>
+            </div>
+        </div>
+            
+             <div class="col-sm-3">
+                 <div class="content">
+                    <div class="content-overlay"></div>
+                <img class="img-responsive rounded mx-auto d-block images" src="image/tinted.jpg" alt="eyeshadow palette" width="200px" height="200px">
+                  <figcaption class="figure-caption"></figcaption>
+                  <div class="content-details fadeIn-bottom">
+                        <p class="content-text">Waterproof Liquid EyeLiner <br> <i class="fa fa-heart-o fa-2x" aria-hidden="true"></i> <i class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i></p>
+                </div>
+            </div>
+            </div>
+            </div>
+
+         
+         <div class="row gallery2">
+            <div class="col-sm-3">
+                <div class="content">
+                <div class="content-overlay"></div>
+                <img class="img-responsive rounded mx-auto d-block images" src="image/moisturizer.jpg" alt="Moisturizer" width="200px" height="200px">
+                <figcaption class="figure-caption"></figcaption>
+                    <div class="content-details fadeIn-top">
+                    <p class="content-text">Daily Hydration Moisturizer<br> <i class="fa fa-heart-o fa-2x" aria-hidden="true"></i> <i class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i></p>
+                    </div>
+                </div>
+            </div>
+             
+             <div class="col-sm-3">
+                 <div class="content">
+                 <div class="content-overlay"></div>
+                <img class="img-responsive rounded mx-auto d-block images" src="image/duoblush.jpg" alt="Moisturizer" width="200px" height="200px">
+                <figcaption class="figure-caption"></figcaption>
+                   <div class="content-details fadeIn-top">
+                    <p class="content-text">Daily Hydration Moisturizer<br> <i class="fa fa-heart-o fa-2x" aria-hidden="true"></i> <i class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i></p>
+                    </div>  
+                </div>
+            </div>
+             
+             <div class="col-sm-3">
+                 <div class="content">
+                 <div class="content-overlay"></div>
+                <img class="img-responsive rounded mx-auto d-block images" src="image/blushBrush.jpg" alt="Moisturizer" width="200px" height="200px">
+                <figcaption class="figure-caption"></figcaption>
+                    <div class="content-details fadeIn-top">
+                    <p class="content-text">Daily Hydration Moisturizer<br> <i class="fa fa-heart-o fa-2x" aria-hidden="true"></i> <i class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i></p>
+                    </div>  
+                </div>
+            </div>
+                 
+                 
+                 
+             <div class="col-sm-3">
+                 <div class="content">
+                 <div class="content-overlay"></div>
+                <img class="img-responsive rounded mx-auto d-block images" src="image/velvet_matte.jpg" alt="Moisturizer" width="200px" height="200px">
+                <figcaption class="figure-caption"></figcaption>
+                 <div class="content-details fadeIn-top">
+                    <p class="content-text">Daily Hydration Moisturizer<br> <i class="fa fa-heart-o fa-2x" aria-hidden="true"></i><i class="fa fa-shopping-cart fa-2x" aria-hidden="true"></i></p>
+                    </div>  
+                </div>
+            </div>     
+         </div>
+<hr style="color: #f1f1f1;">
+    <div class="container">
+         <div class="row justify-content-md-center">
+            <div class="col-sm-9">
+                <img class="rounded gif" src="image/lipstick.gif" width="100%" alt="lipstick gif">
+             </div>
+         </div>
+        </div>
+         
+    <div class="container">
+       <div class="row">
+           <div class="col-md-3">
+           
+           </div>
+           <div class="col-md-3">
+           
+           </div>
+           <div class="col-md-3">
+           
+           </div>
+        </div>  
+    </div>
+    </div>
+    
+   
+<div class="footer">
+    <div class="container-fluid">
+    <div class="float-left"> <p style="line-height: 45px;">&copy; Copyright 2018</p></div>
+        <div class="social float-right">
+            <a href="www.facebook.com"><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a>
+            <a href="www.instagram.com"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+        </div>
+    </div>
+</div>
+</div>
+
+ <script type="text/javascript" src="image/JavaScript/main.js"></script>     
+</body>
+</html>
